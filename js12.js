0eda2280cb25ca9193a3137a45953b51a04f7651
VikramDevireddy <vikramdevireddy888@gmail.com>
2022-10-07 21:25:55 +0530

    hii
    ...

---
+++ js12.js
@@ -0,0 +1,12 @@
+function button_one(){
+    document.getElementById("t1").innerHTML="naku telusu le raa"
+    document.getElementById("t2").innerHTML="good boy oppukunav"
+
+
+}
+function button_two(){
+    document.getElementById("b2").style="padding:10px;"
+    document.getElementById("t1").innerHTML="inkokka chance ista alochinchukuni chepu"
+    document.getElementById("t2").innerHTML="nijam cheppuuu"
+    
+}
